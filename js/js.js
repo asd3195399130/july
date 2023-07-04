fc056c7a158cb09a1c72b154363d83b93a106466
杨浩冰 <3195399130@qq.com>
2023-07-04 09:02:13 +0800

    c

--- js/js.js
+++ js/js.js
@@ -7,6 +7,19 @@
 // let removed = colors.splice(1, 9, "red", "purple"); // 插入两个值，删除一个元素
 // console.log(colors); // red,red,purple,blue
 // console.log(removed); // green，只有一个元素的数组
-let numbers = [1, 2, 3, 4, 5, 4, 3, 2, 1];
-let someResult = numbers.every((item, index, array) => item > 1);
-console.log(someResult); // true
+// let numbers = [1, 2, 3, 4, 5, 4, 3, 2, 1];
+// let someResult = numbers.every((item, index, array) => item > 1);
+// console.log(someResult); // true
+var sex = "男";
+function person() {
+  var name = "张三";
+  function student() {
+    var age = 18;
+    console.log(name); // 张三
+    console.log(sex); // 男
+    // console.log(age);
+  }
+  student();
+  console.log(age); // Uncaught ReferenceError: age is not defined
+}
+person();
